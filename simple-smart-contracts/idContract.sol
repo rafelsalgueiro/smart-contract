ceeb5a382ccc63c3fc0e485ebf267c0c2a8ef1c1
rafelsalgueiro <rss18@alumnes.udl.cat>
2023-02-15 22:41:46 +0100

    try

--- simple-smart-contracts/idContract.sol
+++ simple-smart-contracts/idContract.sol
@@ -4,9 +4,12 @@ pragma solidity ^0.8.10;
 
 contract addingAnId {
     uint256 id = 0;
+    bool itsMine = false;
     
     function setId(uint256 _id) public {
-        id = _id;
+        if (id == _id){
+            itsMine = true;
+        }
     }
 
     function returnId() public view returns (uint256) {
